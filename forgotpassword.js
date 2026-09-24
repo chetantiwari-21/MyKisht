32551125cc15a232d793150b241a70e96ad0872f
Chetan Tiwari <chetantiwari.codes@gmail.com>
2026-09-24 19:24:50 +0530

    Update forgotpassword.js

--- forgotpassword.js
+++ forgotpassword.js
@@ -68,7 +68,7 @@ router.post("/send-otp", async (req, res) => {
         }
 
 
- Generate 6 digit OTP
+// Generate 6 digit OTP
         const otp =
             Math.floor(
                 100000 + Math.random() * 900000
